0a87d04d48f2e842d83c031077cf4646f1d027b5
Sizkal <47240362+Sizkal@users.noreply.github.com>
2019-05-30 17:48:52 +0200

    BoostUI+Clock Started
    jeux c plu programmais

---
+++ Assets/Script/Scripts Marco/Aiguille.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Aiguille : MonoBehaviour
+{
+    public Image _clockHand;
+    public Quaternion _larot;
+
+    private void Start()
+    {
+        _larot = _clockHand.rectTransform.rotation;
+        _larot.z = 100;
+    }
+}
---
+++ Assets/Script/Scripts Marco/BoostUI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoostUI : MonoBehaviour
+{
+    public Image _boostBar;
+    private float _boostAmount;
+    private bool _isBoosting = false;
+
+    private void Start()
+    {
+        _boostAmount = 200f;
+    }
+
+    private void Update()
+    {
+        Debug.Log(_boostAmount);
+        if (_boostAmount < 200f && _isBoosting == false)
+        {
+            _boostAmount = _boostAmount + 10f * Time.deltaTime;
+        }
+        if(_boostAmount <=34)
+        {
+            _boostAmount = 34;
+        }
+        _boostBar.fillAmount = _boostAmount / 200f;
+
+        GoBoost();
+        StopBoost();
+    }
+
+    private void GoBoost()
+    {
+        if (Input.GetKey(KeyCode.G))
+        {
+            _isBoosting = true;
+            _boostAmount = _boostAmount - 1.1f;
+        }
+    }
+
+    private void StopBoost()
+    {
+        if (Input.GetKeyUp(KeyCode.G))
+        {
+            _isBoosting = false;
+        }
+    }
+
+}
