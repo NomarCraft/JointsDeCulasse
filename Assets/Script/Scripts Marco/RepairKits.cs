6c0e760cbeeb6f4f5dc3ef10fad7a8efe2e5b68a
Sizkal <47240362+Sizkal@users.noreply.github.com>
2019-06-03 15:18:03 +0200

    FMOD Setup

--- Assets/Script/Scripts Marco/RepairKits.cs
+++ Assets/Script/Scripts Marco/RepairKits.cs
@@ -6,16 +6,5 @@ using TMPro;
 
 public class RepairKits : MonoBehaviour
 {
-    public GameObject _carController; //Pour récupérer la ref du nombre de kits disponibles
-    public TextMeshProUGUI _kits;
 
-    void Start()
-    {
-        _kits.text = _carController.kits;
-    }
-
-    public virtual void Updatekits(int kits)
-    {
-        _kits.text = kits;
-    }
 }
