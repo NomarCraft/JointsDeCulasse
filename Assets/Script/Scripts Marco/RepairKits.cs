6d45c5637c1e079d570cccc0637a4778ec09065b
Sizkal <47240362+Sizkal@users.noreply.github.com>
2019-06-03 12:56:09 +0200

    UIIntegration_1

---
+++ Assets/Script/Scripts Marco/RepairKits.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class RepairKits : MonoBehaviour
+{
+    public GameObject _carController; //Pour récupérer la ref du nombre de kits disponibles
+    public TextMeshProUGUI _kits;
+
+    void Start()
+    {
+        _kits.text = _carController.kits;
+    }
+
+    public virtual void Updatekits(int kits)
+    {
+        _kits.text = kits;
+    }
+}
