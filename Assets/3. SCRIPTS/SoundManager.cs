49cdf3b5a3d74f022142276c2f41a4e1e070ed6b
Sizkal <47240362+Sizkal@users.noreply.github.com>
2019-06-07 11:14:38 +0200

    MAJFmod 11h14

---
+++ Assets/3. SCRIPTS/SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string _car1Event = "";
+    FMOD.Studio.EventInstance _car1Sound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _car1Sound = FMODUnity.RuntimeManager.CreateInstance(_car1Event);
+        _car1Sound.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
